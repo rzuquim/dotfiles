5d3bfeda16388966a70bdcf8e800297e44d408ea
Rafael Zuquim <rzuquim@gmail.com>
2024-09-23 14:58:13 -0300

    disabling gnome animations

--- pop/gnome_config.sh
+++ pop/gnome_config.sh
@@ -3,6 +3,8 @@
 gsettings set org.gnome.mutter dynamic-workspaces false
 gsettings set org.gnome.desktop.wm.preferences num-workspaces 6
 
+gsettings set org.gnome.desktop.interface enable-animations false
+
 # Disable the default Print Screen shortcut
 gsettings set org.gnome.shell.keybindings show-screenshot-ui '[]'
 gsettings set org.gnome.shell.keybindings screenshot-window '[]'
