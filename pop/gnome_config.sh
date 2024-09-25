38f444c273a1e2bbc01097c07c82ae9873378d7a
Rafael Zuquim <rzuquim@gmail.com>
2024-09-25 17:33:58 -0300

    setting gnome temperature to celcius

--- pop/gnome_config.sh
+++ pop/gnome_config.sh
@@ -10,3 +10,5 @@ gsettings set org.gnome.shell.keybindings show-screenshot-ui '[]'
 gsettings set org.gnome.shell.keybindings screenshot-window '[]'
 gsettings set org.gnome.shell.keybindings screenshot '[]'
 
+gsettings set org.gnome.GWeather temperature-unit centigrade
+
