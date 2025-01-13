8431f0a1d04d3dbf53288c6341edce739b763ca4
Rafael Zuquim <rzuquim@gmail.com>
2025-01-13 20:23:09 -0300

    using custom css to generate pdf from md

--- config/shell/sh/media.sh
+++ config/shell/sh/media.sh
@@ -12,18 +12,13 @@ function md_to_pdf() {
         return 1
     fi
 
-    output_file="${1%.md}.pdf"
-
-    css_file="/tmp/rzuquim/github-markdown.min.css"
-    cdn_url="https://cdnjs.cloudflare.com/ajax/libs/github-markdown-css/5.8.1/github-markdown.min.css"
-
-    if [ ! -f "$css_file" ]; then
-        curl -sSL -o "$css_file" "$cdn_url"
-        if [ $? -ne 0 ]; then
-            echo "Error: Failed to download GitHub CSS file."
-            return 1
-        fi
+    if [[ "$2" == "accessible" ]]; then
+        css_file="$HOME/.config/shell/sh/assets/md-to-pdf-acessible.css"
+    else
+        css_file="$HOME/.config/shell/sh/assets/md-to-pdf.css"
     fi
+
+    output_file="${1%.md}.pdf"
 
     pandoc "$1" -o "$output_file" \
         --css="$css_file" \
