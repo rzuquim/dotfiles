8ef3356848e7da5b143833b2be149ffbfc2bf123
Rafael Zuquim <rzuquim@gmail.com>
2025-03-09 12:39:22 -0300

    script to toggle touchpad

--- config/shell/sh/input-manage.sh
+++ config/shell/sh/input-manage.sh
@@ -18,3 +18,15 @@ function keylang-toggle() {
     esac
 }
 
+function touchpad-toggle() {
+    local TRACKPAD_ID=$(xinput list | /bin/grep -i 'TouchPad\|TrackPad' | /bin/grep -oP 'id=\K\d+')
+    local ENABLED=$(xinput list-props $TRACKPAD_ID | /bin/grep 'Device Enabled' | awk '{print $4}')
+
+    if [[ "$ENABLED" == "1" ]]; then
+        xinput disable $TRACKPAD_ID
+        echo "Trackpad disabled."
+    else
+        xinput enable $TRACKPAD_ID
+        echo "Trackpad enabled."
+    fi
+}
