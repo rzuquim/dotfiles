e30410231fcbadf24772046642740a68d1aa7615
Rafael Zuquim <rzuquim@gmail.com>
2025-04-11 15:30:04 -0300

    asd

--- arch/00_basic.sh
+++ arch/00_basic.sh
@@ -1,3 +1,23 @@
 #!/bin/bash
 
-echo "${BASH_SOURCE[0]}"
+if [[ -n "$ARCH_INTERACTIVE" ]]; then
+    # code to check if the basic config should be applied
+    read -p "Apply basic configs? [y/N] " response
+
+    response=${response:-N}
+    if [[ $response =~ ^[Nn]$ ]]; then
+        ARCH_NO_BASIC=true
+    fi
+fi
+
+
+if [[ -z "$ARCH_NO_BASIC" ]]; then
+    echo -e "${INDIGO}Setting up basic localization${NC}"
+
+    ln -sf /usr/share/zoneinfo/America/Sao_Paulo /etc/localtime
+    hwclock --systohc
+
+    echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
+
+    # more basic stuff here
+fi
