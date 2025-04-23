2d6bfc0129dd81b4e6a20bde41eb3290a0b3d540
Rafael Zuquim <rzuquim@gmail.com>
2025-04-23 15:30:22 -0300

    setup: networking

--- setup/07_network.sh
+++ setup/07_network.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-echo "${BASH_SOURCE[0]}"
+echo -e "${CYAN}Installing network manager${NC}"
+
+pacman -S --noconfirm --needed networkmanager
+systemctl enable NetworkManager
+
+if nmcli -t -f TYPE device | grep -q '^wifi$'; then
+    echo -e "${YELLOW}Turning on WIFI${NC}"
+    nmcli radio wifi on
+fi
+
