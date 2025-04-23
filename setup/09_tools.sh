de7a595e93e705029ded238425cb9244cce4edcb
Rafael Zuquim <rzuquim@gmail.com>
2025-04-23 18:29:42 -0300

    setup: installing basic tools

--- setup/09_tools.sh
+++ setup/09_tools.sh
@@ -1,3 +1,31 @@
 #!/bin/bash
 
-echo "${BASH_SOURCE[0]}"
+BASIC_PACKAGES=(
+    "git"
+    "curl"
+    "7zip"
+    "zip"
+    "unzip"
+    "unrar"
+    "dos2unix"
+    "base-devel"
+    "which"
+    "fd"
+    "fzf"
+    "eza"
+    "ripgrep"
+    "tldr"
+    "tree"
+    "btop"
+    "ncdu"
+    "openvpn"
+    "bat"
+    "wget"
+    "less"
+    "rsync"
+    "git-delta"
+    "wl-clipboard"
+)
+
+echo -e "${CYAN}Installing basic tools:${NC} ${BASIC_PACKAGES[@]}"
+pacman -S --noconfirm --needed ${BASIC_PACKAGES[@]}
