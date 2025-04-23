f0bed2fdf5f43bf7e1733c2a3cbbed2b0be7f742
Rafael Zuquim <rzuquim@gmail.com>
2025-04-23 19:55:00 -0300

    setup: bluetooth

--- setup/16_bluetooth.sh
+++ setup/16_bluetooth.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-echo "${BASH_SOURCE[0]}"
+echo -e "${CYAN}Installing bluetooth${NC}"
+pacman -S --noconfirm --needed bluez bluez-utils
+
+# TODO: bluetuith (AUR)
+#
+if [ ! -f /etc/bluetooth/main.conf.bkp ]; then
+    systemctl enable bluetooth.service
+    cp  /etc/bluetooth/main.conf /etc/bluetooth/main.conf.bkp
+fi
+cp ./_assets/etc/bluetooth/main.conf /etc/bluetooth/main.conf
