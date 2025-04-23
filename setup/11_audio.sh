ad1ec3e5335bbf2ff3115e8729f8fa109993392a
Rafael Zuquim <rzuquim@gmail.com>
2025-04-23 18:32:10 -0300

    setup: audio stuff

--- setup/11_audio.sh
+++ setup/11_audio.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 
-echo "${BASH_SOURCE[0]}"
+echo -e "${CYAN}Setting up audio${NC}"
+pacman -S --noconfirm --needed \
+    pipewire wireplumber pipewire-audio pipewire-pulse pipewire-alsa pipewire-jack gst-plugin-pipewire libpulse \
+    pulsemixer
+
+# NOTE: enbling services
+for user in "${users[@]}"; do
+    user_svcs_folder=/home/$user/.config/systemd/user/default.target.wants
+    mkdir -p $user_svcs_folder
+
+    audio_svcs=(pipewire.service pipewire-pulse.service pipewire-pulse.socket wireplumber.service)
+
+    for svc in "${audio_svcs[@]}"; do
+        ln -sf "/usr/lib/systemd/user/$svc" "$user_svcs_folder/$svc"
+    done
+done
