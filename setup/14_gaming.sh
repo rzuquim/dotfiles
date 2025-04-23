6252226d6b31499da71c775f130e076dca557c3b
Rafael Zuquim <rzuquim@gmail.com>
2025-04-23 18:45:12 -0300

    setup: gaming (if pertinent)

--- setup/14_gaming.sh
+++ setup/14_gaming.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-echo "${BASH_SOURCE[0]}"
+if ! id "fun" &>/dev/null; then
+    echo -e "${YELLOW}Not installing fun stuff.${NC}"
+fi
+
+echo -e "${CYAN}Setting up gaming${NC}"
+pacman -S --noconfirm --needed \
+    steam ttf-liberation gamemode lib32-gamemode mangohud lib32-mangohud vulkan-tools
