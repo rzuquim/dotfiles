778de138440636cc6718232c18ee665bd7458107
Rafael Zuquim <rzuquim@gmail.com>
2025-04-23 13:02:34 -0300

    setup: basic localization

--- setup/01_localization.sh
+++ setup/01_localization.sh
@@ -1,3 +1,31 @@
 #!/bin/bash
 
-echo "${BASH_SOURCE[0]}"
+echo -e "${CYAN}Setting up basic localization${NC}"
+
+if [ ! -e /etc/localtime ]; then
+    echo -e "${YELLOW}Setting up clock${NC}"
+
+    ln -sf /usr/share/zoneinfo/America/Sao_Paulo /etc/localtime
+    hwclock --systohc
+fi
+
+if [ ! -f "/etc/locale.gen.bkp" ]; then
+    echo -e "${YELLOW}Setting up locale${NC}"
+
+    cp /etc/locale.gen /etc/locale.gen.bkp
+    echo "en_US.UTF-8 UTF-8" > /etc/locale.gen
+    echo "pt_BR.UTF-8 UTF-8" >> /etc/locale.gen
+    locale-gen
+
+    echo "LANG=en_US.UTF-8" > /etc/locale.conf
+fi
+
+if [ ! -f "/etc/vconsole.conf" ]; then
+    echo -e "${YELLOW}Setting up console settings${NC}"
+
+    echo "KEYMAP=br-abnt2" > /etc/vconsole.conf
+    echo "FONT=ter-132n" >> /etc/vconsole.conf
+
+    pacman -S --noconfirm --needed terminus-font
+fi
+
