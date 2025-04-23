cab37d6a855b24850e83558b09072e1f3be99bbf
Rafael Zuquim <rzuquim@gmail.com>
2025-04-23 18:30:44 -0300

    setup: installing GPU drivers

--- setup/10_graphics.sh
+++ setup/10_graphics.sh
@@ -1,3 +1,39 @@
 #!/bin/bash
 
-echo "${BASH_SOURCE[0]}"
+gpu_info=$(lspci | grep -Ei "VGA|3D")
+gpu_vendor="unknown"
+
+if echo "$gpu_info" | grep -iq "nvidia"; then
+    gpu_vendor="nvidia"
+elif echo "$gpu_info" | grep -iq "amd"; then
+    gpu_vendor="amd"
+elif echo "$gpu_info" | grep -iq "intel"; then
+    gpu_vendor="intel"
+fi
+
+if [ "$gpu_vendor" = "unknown" ]; then
+    echo -e "${RED}Could not detect the gpu vendor!${NC}"
+    exit 1
+fi
+
+echo -e "${CYAN}Installing graphics drivers for:${NC} $gpu_vendor"
+
+case "$gpu_vendor" in
+    nvidia)
+        pacman -S --noconfirm --needed nvidia-dkms dkms nvidia-utils libva-nvidia-driver lib32-nvidia-utils
+        ;;
+
+    amd)
+        # TODO: there are three implementations, which could be installed simultaneously:
+        #   vulkan-radeon (or lib32-vulkan-radeon) - RADV (part of Mesa project)
+        #   amdvlk (or lib32-amdvlk) - AMDVLK Open (maintained by AMD)
+        #   vulkan-amdgpu-proAUR (or lib32-vulkan-amdgpu-proAUR) - AMDVLK Closed (maintained by AMD)
+        pacman -S --noconfirm --needed \
+            mesa lib32-mesa libva-mesa-driver xf86-video-amdgpu xf86-video-ati vulkan-radeon lib32-vulkan-radeon
+        ;;
+
+    intel)
+        pacman -S --noconfirm --needed  \
+            mesa lib32-mesa libva-intel-driver intel-media-driver vulkan-intel lib32-vulkan-intel
+        ;;
+esac
