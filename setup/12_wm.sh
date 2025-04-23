a474574b9027f58cd2396e0825d3df92572e0d9e
Rafael Zuquim <rzuquim@gmail.com>
2025-04-23 18:33:27 -0300

    setup: basic window manager (hyprland)

--- setup/12_wm.sh
+++ setup/12_wm.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-echo "${BASH_SOURCE[0]}"
+echo -e "${CYAN}Setting up window managers:${NC} hyprland"
+pacman -S --noconfirm --needed \
+    hyprland xdg-desktop-portal-hyprland uwsm wofi \
+    waybar power-profiles-daemon
+
+systemctl enable power-profiles-daemon.service
