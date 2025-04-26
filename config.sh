48b54f43b01cb9a7658955ae2a87e0dced67b2a7
Rafael Zuquim <rzuquim@gmail.com>
2025-04-26 19:36:13 -0300

    config: custom configs for each user

--- config.sh
+++ config.sh
@@ -28,7 +28,7 @@ rsync -avh --no-perms ./config/shared/ /home/.shared
 users=(me stream fun)
 
 for user in "${users[@]}"; do
-    if ! id $user; then
+    if ! id $user &> /dev/null; then
         continue
     fi
 
@@ -70,11 +70,40 @@ for user in "${users[@]}"; do
     done
 done
 
+for user in "${users[@]}"; do
+    if ! id $user &> /dev/null; then
+        continue
+    fi
+
+    user_home="/home/$user"
+    if sudo test ! -d "$user_home/.config"; then
+        continue
+    fi
+
+    if [ ! -d "./config/$user/config" ]; then
+        continue
+    fi
+
+    find ./config/$user/config -type f | while read -r user_config; do
+        rel_path="${user_config#./config/$user/config/}"
+        target="$user_home/.config/$rel_path"
+        target_dir="$(dirname "$target")"
+
+        if sudo test ! -e "$target_dir"; then
+            sudo mkdir -p "$target_dir"
+        fi
+
+        abs_path_to_config=$(realpath "$user_config")
+        echo -e "${VIOLET}copying custom config file${NC} $user_config ${VIOLET}into${NC} $target"
+        sudo cp "$abs_path_to_config" "$target"
+        sudo chown -h "$user:$user" "$target"
+    done
+done
+
 echo
 echo "-----------------"
 echo -e "${CYAN}Syncing nvim config${NC}"
 echo "-----------------"
-
 
 if [ ! -L ~/.config/nvim ]; then
     if [ ! -d ~/nvim ]; then
