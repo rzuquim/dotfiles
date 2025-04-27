6f3e57649cd28ec5b91aa45e519ca9cc3a417c3b
Rafael Zuquim <rzuquim@gmail.com>
2025-04-27 15:49:37 -0300

    config: fixing hyprland script to open browser

--- config/shared/config/hypr/scripts/browser.sh
+++ config/shared/config/hypr/scripts/browser.sh
@@ -2,8 +2,10 @@
 
 curr_workspace=$(hyprctl activeworkspace | sed -n 's/.*(\(.*\)).*/\1/p')
 
-if [ "$curr_workspace" = "web" ]; then
-    librewolf
-else
+if [ "$curr_workspace" != "web" ]; then
     hyprctl dispatch workspace name:web
+fi
+
+if ! pgrep -x librewolf > /dev/null; then
+    librewolf -P default
 fi
