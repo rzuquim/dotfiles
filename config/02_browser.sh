bae7ec991cd47575ed3138c10de1728f604617b1
Rafael Zuquim <rzuquim@gmail.com>
2025-04-27 18:49:46 -0300

    config: browser permissions file and addons TODO

--- config/02_browser.sh
+++ config/02_browser.sh
@@ -16,7 +16,7 @@ fi
 
 mkdir -p ~/.librewolf/default
 
-if [ -f ~/.librewolf/profiles.ini ]; then
+if [ ! -L ~/.librewolf/profiles.ini ]; then
     echo -e "${RED}Deleting old ~/.librewolf config.${NC}"
     rm -rf ~/.librewolf/*
 fi
@@ -32,7 +32,36 @@ if [ ! -L ~/.librewolf/default/user.js ]; then
     ln -s ~/Config/librewolf/default/user.js ~/.librewolf/default/user.js
 fi
 
-if [ ! -L ~/.librewolf/default/addons.json ]; then
-    echo "Linking ~/Config/librewolf/default/addons.json" 
-    ln -s ~/Config/librewolf/default/addons.json ~/.librewolf/default/addons.json
-fi
+# TODO: permissions white list
+# if [ ! -L ~/.librewolf/default/permissions.sqlite ]; then
+#     echo "Linking ~/Config/librewolf/default/permissions.sqlite" 
+#     ln -s ~/Config/librewolf/default/permissions.sqlite ~/.librewolf/default/permissions.sqlite
+# fi
+
+# TODO: install extensions
+# if [ ! -L ~/.librewolf/default/addons.json ]; then
+#     echo "Linking ~/Config/librewolf/default/addons.json" 
+#     ln -s ~/Config/librewolf/default/addons.json ~/.librewolf/default/addons.json
+# fi
+#
+# EXTENSIONS_DIR="$HOME/.librewolf/default/extensions"
+# if [ ! -e "$EXTENSIONS_DIR" ]; then
+#     mkdir -p "$EXTENSIONS_DIR"
+#
+#     echo "${CYAN}Downloading and installing extensions...${NC}"
+#     declare -A EXTENSIONS=(
+#         ["addon@darkreader.org"]="https://addons.mozilla.org/firefox/downloads/file/4229264/darkreader-4.9.76.xpi"
+#         ["addon@vimium"]="https://addons.mozilla.org/firefox/downloads/file/4458679/vimium_ff-2.2.1.xpi"
+#     )
+#
+#     for addon_id in "${!EXTENSIONS[@]}"; do
+#         echo "Installing $addon_id..."
+#         wget -O "$EXTENSIONS_DIR/$addon_id.xpi" "${EXTENSIONS[$addon_id]}"
+#
+#         if [ $? -eq 0 ]; then
+#             echo "Successfully installed $addon_id"
+#         else
+#             echo "Failed to install $addon_id"
+#         fi
+#     done
+# fi
