806c92e37a844babf0201651d8f8ff07ab456608
Rafael Zuquim <rzuquim@gmail.com>
2025-05-03 03:15:42 -0300

    config: grouping chats

---
+++ config/shared/config/hypr/scripts/open_chats.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+wasistlos &
+telegram-desktop &
+
+chats=("wasistlos" "org.telegram.desktop")
+
+wait_for_windows() {
+    local i=0
+    while true; do
+        found_all=true
+        for app in "${chats[@]}"; do
+            if ! hyprctl clients -j | grep -q "$app"; then
+                found_all=false
+                break
+            fi
+        done
+        $found_all && break
+        sleep 0.5
+        i=$((i + 1))
+        if [ i -gt 10 ]; then
+            echo "Timeout waiting for chats to appear"
+            exit -1
+        fi
+    done
+}
+
+wait_for_windows
+
+addresses=()
+for app in "${chats[@]}"; do
+    address=$(hyprctl clients -j | jq -r ".[] | select(.class == \"${app}\") | .address")
+    [ -n "$address" ] && addresses+=("$address")
+done
+
+if [ ${#addresses[@]} -ge 2 ]; then
+    hyprctl dispatch focuswindow address:${addresses[0]}
+    hyprctl dispatch togglegroup
+
+    for ((i = 1; i < ${#addresses[@]}; i++)); do
+        hyprctl dispatch focuswindow address:${addresses[i]}
+        hyprctl dispatch togglegroup
+        hyprctl dispatch moveintogroup l
+    done
+
+    # go back to main
+    hyprctl dispatch activeworkspace name:main
+fi
+
