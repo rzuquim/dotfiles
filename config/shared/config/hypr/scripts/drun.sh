f6e41b766437ddc28453fa96392612c499a4ee3c
Rafael Zuquim <rzuquim@gmail.com>
2025-05-19 20:24:14 -0300

    config: fixing drun issue on empty workspace

--- config/shared/config/hypr/scripts/drun.sh
+++ config/shared/config/hypr/scripts/drun.sh
@@ -3,11 +3,14 @@
 killall wofi
 
 active_window=$(hyprctl activewindow -j)
-is_fullscreen=$(echo "$active_window" | jq '.fullscreen')
-if [ "$is_fullscreen" != "0" ]; then
-    hyprctl dispatch fullscreen 2
-fi
 
-source "$HOME/.config/hypr/scripts/ensure_waybar.sh"
+if [ "$active_window" != "{}" ]; then
+    is_fullscreen=$(echo "$active_window" | jq '.fullscreen')
+    if [ "$is_fullscreen" != "0" ]; then
+        hyprctl dispatch fullscreen 2
+    fi
+
+    source "$HOME/.config/hypr/scripts/ensure_waybar.sh"
+fi
 
 wofi --show drun --insensitive --matching=fuzzy
