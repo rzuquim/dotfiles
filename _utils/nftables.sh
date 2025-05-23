4a0bfcc94d6c913c6465e343ee6ecdd4f63398d7
Rafael Zuquim <rzuquim@gmail.com>
2025-05-23 18:19:04 -0300

    asd

--- _utils/nftables.sh
+++ _utils/nftables.sh
@@ -8,14 +8,23 @@ function nft_rule_add() {
     local output_file="/tmp/nftables.conf"
 
     # Use awk to replace the placeholder with the rule block
-    awk -v rules="$nftables_rules" -v tag="$placeholder" '
-    {
-        if ($0 ~ "# " tag) {
-            print rules
-        } else {
-            print $0
-        }
-    }' "$input_file" > "$output_file"
+    # awk -v rules="$nftables_rules" -v tag="$placeholder" '
+    # {
+    #     if ($0 ~ "# " tag) {
+    #         print rules
+    #     } else {
+    #         print $0
+    #     }
+    # }' "$input_file" > "$output_file"
+
+    awk -v tag="$placeholder" -v rfile="$rules_file" '
+    $0 ~ tag {
+        while ((getline line < rfile) > 0) print line
+        close(rfile)
+        next
+    }
+    { print }
+    ' "$input_file" > "$output_file"
 
     sudo cp /etc/nftables.conf /etc/nftables.conf.bkp
     # sudo mv /tmp/nftables.conf /etc/nftables.conf
