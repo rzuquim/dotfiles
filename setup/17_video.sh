8d3b93fa80a97489ddfaf3acfec2abd11ed9bc51
Rafael Zuquim <rzuquim@gmail.com>
2025-06-19 10:19:18 -0300

    wip

--- setup/17_video.sh
+++ setup/17_video.sh
@@ -8,3 +8,14 @@ VIDEO_PACKAGES=(
 echo -e "${CYAN}Installing video tools:${NC} ${VIDEO_PACKAGES[@]}"
 pacman -S --noconfirm --needed ${VIDEO_PACKAGES[@]}
 
+if ! id "stream" &>/dev/null; then
+    echo -e "${YELLOW}Not installing streaming stuff.${NC}"
+    return
+fi
+
+STREAMING_PACKAGES=(
+    "obs-studio"
+)
+
+echo -e "${CYAN}Installing streaming tools:${NC} ${STREAMING_PACKAGES[@]}"
+pacman -S --noconfirm --needed ${STREAMING_PACKAGES[@]}
