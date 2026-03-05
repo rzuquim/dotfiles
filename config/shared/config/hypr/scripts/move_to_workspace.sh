ff4f7f385b1db44c40e46ec7492c7abebc7ae9ad
Rafael Zuquim <rzuquim@gmail.com>
2026-03-05 11:47:56 -0300

    move to specific workspace on hyprland

---
+++ config/shared/config/hypr/scripts/move_to_workspace.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+WIN_ADDR=$(hyprctl activewindow -j | jq -r '.address')
+
+if [[ -z "$WIN_ADDR" || "$WIN_ADDR" == "null" ]]; then
+    exit 0
+fi
+
+WORKSPACE_LIST=$(hyprctl workspaces -j | jq -r '.[] | "\(.name)"')
+
+CHOICE=$(\
+    echo "$WORKSPACE_LIST" | \
+    wofi --dmenu --prompt "Move window to workspace")
+
+if [[ -z "$CHOICE" ]]; then
+    exit 0
+fi
+
+hyprctl dispatch movetoworkspace "name:$CHOICE,address:$WIN_ADDR"
