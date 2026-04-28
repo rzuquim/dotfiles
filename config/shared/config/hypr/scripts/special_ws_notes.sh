668759297298446e1057ff1c9b4ea5ef153d06da
Rafael Zuquim <rzuquim@gmail.com>
2026-04-28 08:33:45 -0300

    setting working dir in notes

--- config/shared/config/hypr/scripts/special_ws_notes.sh
+++ config/shared/config/hypr/scripts/special_ws_notes.sh
@@ -4,5 +4,5 @@ SCRIPT_DIR="$(dirname "$(realpath "$0")")"
 source "$SCRIPT_DIR/special_ws.sh"
 
 ws_class=notes
-project="nvim $HOME/Personal/notes"
-open_special_ws $ws_class "alacritty --class $ws_class -e ${project}"
+project="nvim ."
+open_special_ws $ws_class "alacritty --class $ws_class --working-directory "$HOME/Personal/notes" -e ${project}"
