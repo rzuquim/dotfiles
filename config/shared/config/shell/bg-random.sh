01cf850721b63b0358515c8d000347e098484a56
Rafael Zuquim <rzuquim@gmail.com>
2026-05-27 12:08:19 -0300

    fix: random-bg wallpaper broke with hyprpaper syntax change

--- config/shared/config/shell/bg-random.sh
+++ config/shared/config/shell/bg-random.sh
@@ -3,6 +3,5 @@
 function bg-random() {
     WALLPAPER_DIR="/home/.shared/wallpapers"
     WALLPAPER=$(find "$WALLPAPER_DIR" -type f | shuf -n 1)
-    hyprctl hyprpaper reload ,"$WALLPAPER"
+    hyprctl hyprpaper wallpaper ", $WALLPAPER,"
 }
-
